0eb23e8de0a074e25ccb8def14ecd3dbaceb5ffe
Joanna Edgell <joedgell2013@gmail.com>
2014-10-01 15:46:29 -0400

    working on 2

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ var total = priceArray.reduce(function(x,y){
 var avg = total / 25;
 
 console.log('The average price is $' + avg.toFixed(2));
+
+//Array of items that cost between $14 & $18
+
+var limited = priceArray.filter (function (num) {
+  return num >= 14 && num <= 18;
+});
+
+
+
+console.log (limited);
